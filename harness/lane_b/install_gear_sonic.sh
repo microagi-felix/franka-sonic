296f1451920709dd22875947d159bac625b82fd8
Felix Minzenmay <felix.minze@live.de>
2026-09-03 16:58:33 +0000

    P5: hold-test diagnostic (decoder cannot hold L_j7 yet); jp11 = jp7 with joint signal x1.5-2

--- harness/lane_b/install_gear_sonic.sh
+++ harness/lane_b/install_gear_sonic.sh
@@ -56,6 +56,7 @@ install_file "$HERE/sonic_dual_fr3_jp7.yaml" "$GS/config/exp/sonic_dual_fr3_jp7.
 install_file "$HERE/sonic_dual_fr3_jp8.yaml" "$GS/config/exp/sonic_dual_fr3_jp8.yaml"
 install_file "$HERE/sonic_dual_fr3_jp9.yaml" "$GS/config/exp/sonic_dual_fr3_jp9.yaml"
 install_file "$HERE/sonic_dual_fr3_jp10.yaml" "$GS/config/exp/sonic_dual_fr3_jp10.yaml"
+install_file "$HERE/sonic_dual_fr3_jp11.yaml" "$GS/config/exp/sonic_dual_fr3_jp11.yaml"
 
 # 4. robot_mapping entry + order converter (minimal in-place patches, idempotent)
 python3 - "$GS" <<'PY'
